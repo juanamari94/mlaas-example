c9d169c24e235c5846b035fc102ee9718cc72d08
Juan Amari <juan.amari@redb.ee>
2017-06-30 10:16:26 -0300

    deleted old code and imports, put table creation in its own method

--- main.py
+++ main.py
@@ -4,32 +4,17 @@ from flask import Flask, render_template, request, flash, abort
 import csv
 import numpy as np
 from sklearn import linear_model
-from sklearn import tree
-from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn import svm
 from sklearn.metrics import f1_score
 from werkzeug.utils import secure_filename
 from supervised_binary_classification_model import SupervisedBinaryClassificationModel
 
-dest_file = 'data.csv'
 UPLOAD_FOLDER = os.getcwd() + '/datasets'
 ALLOWED_EXTENSIONS = {'csv'}
 training_set_ratio = 80
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-models = {'Logistic Regression': linear_model.LogisticRegression(),
-          'Decision Tree': tree.DecisionTreeClassifier(),
-          'Gaussian Naive Bayes': GaussianNB(),
-          'Ada Boost Decision Tree Ensemble': AdaBoostClassifier(),
-          'Multi-Layer Perceptron': MLPClassifier(solver='lbfgs',
-                                                  alpha=1e-5,
-                                                  hidden_layer_sizes=(5, 2),
-                                                  random_state=1),
-          }
 
 
 def load_data(filename):
@@ -39,37 +24,19 @@ def load_data(filename):
 		return raw_data
 
 
-def machine_learning():
-	with open(dest_file, 'r') as dest_f:
-		data_iter = csv.reader(dest_f, delimiter=',')
-		raw_data = [data for data in data_iter]
-
-	# Data preparation
-	[row.pop(0) for row in raw_data]  # Remove unnecessary column
-	column_names = raw_data.pop(0)
-	raw_labels = [row.pop(0) for row in raw_data]
-	labels = np.array(list(
-		map(lambda x: 0 if x == 'B' else 1, raw_labels)))  # Map it and turn it to a list. 0 for bening, 1 for malignant
-	features = np.array(raw_data, dtype='float')
-
-	for model_name, model in models.items():
-		results = []
-		results_f1 = []
-		for i in range(0, 10000):
-			x_train, x_test, y_train, y_test = train_test_split(features, labels)
-
-			# Model training and performance testing
-			model.fit(x_train, y_train)
-			results.append(model.score(x_test, y_test))
-			predictions = model.predict(x_test)
-			f1score = f1_score(y_test, predictions)
-			results_f1.append(f1score)
-		print("Model: ", model_name)
-		print("max: ", max(results))
-		print("min: ", min(results))
-		print("mean: ", sum(results) / 10000)
-		print("f1 score mean: ", sum(results_f1) / 10000)
-
+def create_html_table(predictions, model):
+	table = '<table><tr>'
+	for column_name in model.column_names:
+		table += '<th>' + column_name + '</th>'
+	table += '</tr>'
+	for tup in predictions:
+		table += "<tr><td>" + str(model.classes[tup[0]]) + "</td>"
+		for feature in tup[1]:
+			table += "<td>" + str(feature) + "</td>"
+		table += "</tr>"
+	table += '<tr><td> Precisión: ' + str(model.accuracy_metrics()) + '</td></tr>'
+	table += '<tr><td> Puntaje F1: ' + str(model.calculate_f1_score()) + '</td></tr></table>'
+	return table
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -125,17 +92,8 @@ def upload_file():
 			classifier = SupervisedBinaryClassificationModel(raw_training_data, raw_prediction_data, \
 			                                                 linear_model.LogisticRegression())
 			classifier.train()
-			print(raw_prediction_data)
 			predictions = classifier.predict()
-			result = '<table><tr>'
-			for column_name in classifier.column_names:
-				result += '<th>' + column_name + '</th>'
-			result += '</tr>'
-			for tup in predictions:
-				result += "<tr><td>" + str(classifier.classes[tup[0]]) + "</td>"
-				for feature in tup[1]:
-					result += "<td>" + str(feature) + "</td>"
-				result += "</tr>"
+			result = create_html_table(predictions, classifier)
 			return result
 		elif function == 'estimate':
 			print("b")
